eb19d58f19c628d0e73571bd6afe22dc3e1f3014
Christopher Clure <clurec@gmail.com>
2024-07-14 13:54:22 -0700

    Solves Problem 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  charArray = string.split("");
+  reverseArray = [];
+  for (let i = charArray.length - 1; i>=0; i--) {
+    reverseArray.push(charArray[i]);
+  }
+  return reverseArray.join("");
 };
 
 // Do not edit below this line
