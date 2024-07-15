0b1281d27742603de9eaa1a9de3fb0dfbddb829a
Christopher Clure <clurec@gmail.com>
2024-07-14 22:45:25 -0700

    Solves problem 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(enteredArray) {
+  for (let i = 1 ; i < arguments.length ; i++) {
+    while (true) {
+    let index = enteredArray.indexOf(arguments[i]);
+    if (index === -1) {
+      break;
+    }
+    enteredArray.splice(index, 1);
+    }
+  };
+  return enteredArray;
 };
 
 // Do not edit below this line
