26b6e088b514dbd38ca7a55368ccfc9a7abf437b
Christopher Clure <clurec@gmail.com>
2024-07-14 23:05:49 -0700

    Solves problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (var1, var2) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  if (var1 < 0 || var2 < 0 || typeof var1 != "number" || typeof var2 != "number") {
+    return "ERROR";
+  }
+  if (var1 < var2) {
+    start = var1;
+    end = var2;
+  } else {
+    start = var2;
+    end = var1;
+  }
+  for (let i = start; i <= end; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
